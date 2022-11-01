d77bab5be42270f35dfeab6f04701fe2d9e1122c
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-11-01 16:45:45 -0500

    Completed fetching the weather for a city query.

--- src/App.js
+++ src/App.js
@@ -68,8 +68,8 @@ export default function App() {
           <div className="date">{dateBuilder(new Date())}</div>
         </div>
         <div className="weather-box">
-          <div className="temp">15°C</div>
-          <div className="weather">Sunny</div>
+          <div className="temp">{Math.round(weather.main.temp)}°C</div>
+          <div className="weather">{weather.weather[0].main}</div>
         </div>
         </div>
         ) : ('')}
